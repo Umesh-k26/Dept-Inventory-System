3c22521f4b8f5a1dceb3c80b24fa07d1cb8f5c03
revanthbadavathu <revanthbadavathu@gmail.com>
2023-04-29 21:55:06 +0530

    update

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -66,6 +66,21 @@ def test_add_user():
     )
     assert res.status_code == 200
     # assert res.json() == {"detail": "user added"}
+    
+    res = client.post(
+        "/add-user",
+        headers={"Authorization": "cs20btech11007@iith.ac.in"},
+        json=User(
+            user_id="cs20btech11007",
+            first_name="B.Revanth",
+            last_name=None,
+            email="cs20btech11007@iith.ac.in",
+            # department="CSE",
+            user_state="Active",
+            user_type="Admin",
+        ).dict(),
+    )
+    assert res.status_code == 200
 
     # same user_id
     res = client.post(
@@ -100,6 +115,39 @@ def test_add_user():
     )
     assert res.status_code == 400
     # assert res.json() == {"detail": "Cant add user"}
+    
+    #same user_id and same email_id as student
+    res = client.post(
+        "/add-user",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        json=User(
+            user_id="cs20btech11008",
+            first_name="Raja",
+            last_name="ravi kiran",
+            email="cs20btech11008@iith.ac.in",
+            department="CSE",
+            user_state="Active",
+            user_type="student",
+        ).dict(),
+    )
+    assert res.status_code == 200
+    # assert res.json() == {"detail": "user added"}
+    
+    res = client.post(
+        "/add-user",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        json=User(
+            user_id="cs20btech11008",
+            first_name="Raja",
+            last_name="ravi kiran",
+            email="cs20btech11008@iith.ac.in",
+            department="CSE",
+            user_state="Active",
+            user_type="student",
+        ).dict(),
+    )
+    assert res.status_code == 400
+    # assert res.json() == {"detail": "cant add user"}
 
 
 def test_update_user():
@@ -109,6 +157,18 @@ def test_update_user():
         json=User(
             user_id="cs20btech11014",
             last_name="Goyal",
+        ).dict(),
+    )
+    assert res.status_code == 200
+    assert res.json() == {"detail": "User Updated"}
+    
+    res = client.put(
+        "/update-user",
+        headers={"Authorization": "cs20btech11007@iith.ac.in"},
+        json=User(
+            user_id="cs20btech11007",
+            last_name="Nayak",
+            user_type="stud",
         ).dict(),
     )
     assert res.status_code == 200
@@ -134,7 +194,14 @@ def test_activate_deactivate_user():
     )
     assert res.status_code == 200
     assert res.json() == {"detail": "user state changed"}
-
+    
+    res = client.put(
+        "/activate-deactivate-user/cs20btech11024/Active",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+    )
+    assert res.status_code == 200
+    assert res.json() == {"detail": "user state unchanged"}
+    
     res = client.put(
         "/activate-deactivate-user/cs20btech11000/Active",
         headers={"Authorization": "cs20btech11024@iith.ac.in"},
@@ -149,10 +216,17 @@ def test_filter_users():
         headers={"Authorization": "cs20btech11024@iith.ac.in"},
         json=User(user_type="Stud", department="CSE").dict(),
     )
-    data = res.json()
     assert res.status_code == 200
-    for user in data:
-        print(user)
+    print(res.json)
+    
+    res = client.post(
+        "/get-user",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        json=User(user_type="Faculty", department="CSE").dict(),
+    )
+    assert res.status_code == 200
+    print(res.json)
+    
 
 
 # Order Testing
@@ -173,6 +247,39 @@ def test_add_order():
     assert res.status_code == 200
     # assert res.json() == {"detail": "asset added"}
     print(res.json())
+    
+    res = client.post(
+        "/add-order",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        json=Order_Table(
+            purchase_order_no="1",
+            financial_year=2023,
+            indentor="cs20btech11014",
+            total_price=2500,
+            invoice_date='2023-05-2',
+        ).dict(),
+        files=(),
+    )
+
+    assert res.status_code == 200
+    # assert res.json() == {"detail": "asset added"}
+    print(res.json())
+    
+    #adding the existing iteam in table
+    res = client.post(
+        "/add-order",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        json=Order_Table(
+            purchase_order_no="1",
+            financial_year=2023,
+            indentor="cs20btech11014",
+        ).dict(),
+        files=(),
+    )
+
+    assert res.status_code == 400
+    #assert res.json()=={"detail":"asset already added"}
+    print(res.json)
 
 
 # Asset Testing
@@ -189,6 +296,27 @@ def test_add_asset():
             asset_holder="cs20btech11014",
             purchase_order_no="1",
             financial_year=2023,
+            # picture=None,
+        ).dict(),
+        files=(),
+    )
+
+    assert res.status_code == 200
+    # assert res.json() == {"detail": "asset added"}
+    print(res.json())
+
+    res = client.post(
+        "/add-asset",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        data=Asset(
+            asset_name="Mouse",
+            model="HP M270",
+            serial_no="173",
+            asset_holder="cs20btech11014",
+            purchase_order_no="1",
+            financial_year=2023,
+            entry_date='2023-05-2',
+            Warranty='2024-04-04',
             # picture=None,
         ).dict(),
         files=(),
@@ -231,6 +359,18 @@ def test_filter_asset():
     for asset in data:
         print(asset)
 
+    res = client.post(
+        "/get-asset",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        json=Asset(
+            asset_name="Mousepad",
+        ).dict(),
+    )
+    data = res.json()
+    assert res.status_code == 200
+    print(data)
+    # assert res.json() == {"detail": "asset not found"}
+
 
 # Bulk Asset Testing
 
@@ -245,6 +385,22 @@ def test_add_bulk_asset():
             serial_no="154",
             purchase_order_no="1",
             financial_year=2023,
+            # picture=None,
+        ).dict(),
+        files=(),
+    )
+
+    res = client.post(
+        "/add-bulk-asset",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        data=Bulk_Asset(
+            asset_name="Dining-Table",
+            serial_no="456",
+            purchase_order_no="1",
+            financial_year=2023,
+            deparment="CSE",
+            quantity=1,
+            entry_date='2023-05-02',
             # picture=None,
         ).dict(),
         files=(),
@@ -285,3 +441,15 @@ def test_filter_bulk_asset():
     assert res.status_code == 200
     for asset in data:
         print(asset)
+
+    res = client.post(
+        "/get-bulk-asset",
+        headers={"Authorization": "cs20btech11024@iith.ac.in"},
+        json=Bulk_Asset(
+            asset_name="Sofa",
+        ).dict(),
+    )
+    data = res.json()
+    assert res.status_code == 200
+    print(data)
+    # assert res.json() == {"detail": "Bulk asset not found"}
